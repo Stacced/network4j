8ae02f453920094c5b889f0443253132ba6d3bc8
Adamo-C <adamo.cannone@etu.hesge.ch>
2021-12-12 21:31:25 +0100

    Implementation requetes (Insert manquant)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,11 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("#### Bienvenue sur Network4J ####");
         Scanner in = new Scanner(System.in);
+        String userCommand;
 
         System.out.print("> Veuillez entrer l'URI de connexion à votre base de données Neo4J: ");
         String neoUri = in.nextLine();
@@ -19,6 +21,41 @@ public class Main {
             System.out.printf("[ERR] Erreur lors de la connexion à la base de données: %s", e.getMessage());
             return;
         }
+
+        do {
+            System.out.println();
+            System.out.println("> Veuillez entrer la commande à executer");
+            System.out.println("  > RTR : Récupérer tous les routeurs sortant d'un datacenter (relations, interfaces et zones réseaux aussi)");
+            System.out.println("  > AR : Récupérer tous les racks d'un datacenter dont les switches liés, avec les interfaces connectées et la zone IP associée");
+            System.out.println("  > I-RTR : Insérer un routeur de sortie d'un datacenter");
+            System.out.println("  > CR : Calculer la route la plus efficace pour aller d'un rack à un routeur de sortie");
+            System.out.println("  > EXIT : Quitter et se déconnecter");
+
+            userCommand = in.nextLine();
+
+            switch (userCommand.toUpperCase(Locale.ROOT)) {
+                case "RTR":
+                    Neo4jDatabase.getRoutersOut();
+                    break;
+                case "AR":
+                    Neo4jDatabase.getAllRacks();
+                    break;
+                case "I-RTR":
+                    System.out.println("1");
+                    break;
+                case "CR":
+                    System.out.print("> Veuillez indiquer le numéro du rack de départ: ");
+                    String rack = in.nextLine();
+
+                    System.out.print("> Veuillez indiquer le numéro de la zone du rack: ");
+                    String zone = in.nextLine();
+
+                    Neo4jDatabase.calcShortestPath(rack, zone);
+                    break;
+            }
+
+        } while(!userCommand.equals("EXIT"));
+
 
         System.out.println("#### Merci d'avoir utilisé Network4J ####");
         // Close database session and connection
--- src/main/java/Neo4jDatabase.java
+++ src/main/java/Neo4jDatabase.java
@@ -1,5 +1,11 @@
+import com.sun.source.tree.LiteralTree;
 import org.neo4j.driver.*;
+import org.neo4j.driver.Record;
 import org.neo4j.driver.exceptions.Neo4jException;
+
+import java.nio.file.attribute.UserPrincipalNotFoundException;
+import java.util.HashMap;
+import java.util.List;
 
 public class Neo4jDatabase {
     private static Driver conn;
@@ -8,10 +14,80 @@ public class Neo4jDatabase {
         conn = GraphDatabase.driver(uri, AuthTokens.basic(user, password));
     }
 
-    public static Result query(String cypher) {
+
+    public static void addEgressRouter() {
+
+    }
+
+    public static void calcShortestPath(String rack, String zone) {
+
+
+        String cypherQuery = "MATCH path = allShortestPaths( (rack:Rack{zone:1,rack:1})-[:HOLDS|ROUTES|CONNECTS*]-(router:Router:Egress) )\n" +
+                "RETURN length(path) as hops, count(*) as count, rack.name as rack, router.name as rtr, router.ip as ip_rtr;";
+
+        List<Record> result = query(cypherQuery);
+
+        System.out.println();
+
+
+        for (Record record : result) {
+            System.out.println("Chemin le plus cours pour le rack " + record.get("rack").asString() + "\n" +
+                    "Routeur destination : " + record.get("rtr").asString() + " (IP: " + record.get("ip_rtr").asString() + ")\n" +
+                    "Longueur du chemin : " + record.get("hops").asInt());
+        }
+
+        System.out.println();
+    }
+
+
+    public static void getAllRacks() {
+        String cypherQuery = "MATCH (dc:DataCenter {name:\"DC1\"})-[:CONTAINS]->(rack:Rack)-[:HOLDS]->(s:Switch)-[:ROUTES]->(si:Interface)<-[:ROUTES]-(nr:Network:Zone)\n" +
+                "RETURN rack.rack as no_rack, rack.name as rack, s.ip as ip_sw, si.ip as ip_con_int, nr.ip as network;";
+
+        List<Record> result = query(cypherQuery);
+
+        System.out.println();
+
+
+        for (Record record : result) {
+            System.out.println("Rack: " + record.get("rack").asString() + " :\n" +
+                    "   Sous-réseaux du switch: " + record.get("ip_sw").asString() + ".0\n" +
+                    "   Interface connectée au switch: " + record.get("ip_con_int").asString() + " (Zone: " + record.get("network").asString() + ".0.0" + ")");
+            System.out.println();
+        }
+
+        System.out.println();
+    }
+
+
+    public static void getRoutersOut() {
+
+
+            String cypherQuery = "MATCH (dc:DataCenter {name: \"DC1\"})-[:CONTAINS]->(r:Router:Egress)-[:ROUTES]->(i:Interface)-[:CONNECTS]->(nw:Network)<-[:CONNECTS]-(i2:Interface)<-[:ROUTES]-(rin:Router)" +
+                    "return i.ip as ip_rtr_out, nw.ip as network, r.name as rtr_out, rin.name as rtr_inside, i2.ip as ip_rtr_inside;";
+
+            List<Record> result = query(cypherQuery);
+
+            System.out.println();
+
+            for (Record record : result) {
+                System.out.println("Nom du routeur : " + record.get("rtr_inside").asString() +
+                        " (IP: " + record.get("ip_rtr_inside") .asString() +
+                        " Zone: " + record.get("network").asString() + ".0.0" + ")" +
+                        " via le routeur " + record.get("rtr_out").asString() +
+                        " (IP: " + record.get("ip_rtr_out").asString() + ")");
+            }
+
+            System.out.println();
+    }
+
+
+    private static List<Record> query(String cypher) {
         try (Session session = conn.session()) {
-            // TODO: use transactions ?
-            return session.run(cypher);
+
+            Result r = session.run(cypher);
+            return r.list();
+
         } catch (Neo4jException ex) {
             System.out.println("[ERR] " + ex);
         }
